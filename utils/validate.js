500862ea1a9bae5bac295db74479eaaa0c152bb6
Eric Fortis <efortis@gmail.com>
2024-07-02 00:47:01 -0400

    fix validator

--- utils/validate.js
+++ utils/validate.js
@@ -1,17 +1,27 @@
+const isTypeOf = example => value =>
+	Object.prototype.toString.call(value) ===
+	Object.prototype.toString.call(example)
+
+const typeCheckers = new Map([
+	[Date, isTypeOf(new Date())],
+	[Array, isTypeOf([])],
+	[String, isTypeOf('')],
+	[Object, isTypeOf({})],
+	[Number, isTypeOf(1)],
+	[RegExp, isTypeOf(/a/)],
+	[Boolean, isTypeOf(true)],
+	[Function, isTypeOf(a => a)]
+])
+
+
 export function validate(obj, shape) {
 	for (const [field, value] of Object.entries(obj)) {
 		const validator = shape[field]
-		if (isTypeOf(validator, Function) && validator !== Function) {
-			if (!validator(value))
+		if (typeCheckers.has(validator)) {
+			if (!typeCheckers.get(validator)(value))
 				throw new TypeError(`${field} ${value}`)
 		}
-		else if (!isTypeOf(validator, value))
+		else if (!validator(value))
 			throw new TypeError(`${field} ${value}`)
 	}
-}
-
-function isTypeOf(example, value) {
-	return (
-		Object.prototype.toString.call(value) ===
-		Object.prototype.toString.call(example))
 }

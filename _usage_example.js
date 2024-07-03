033e7cc58435c24d34b7c932b3209404c3fb1db6
Eric Fortis <efortis@gmail.com>
2024-07-03 12:06:09 -0400

    demo cookie

--- _usage_example.js
+++ _usage_example.js
@@ -10,6 +10,9 @@ Mockaton({
 	cookies: {
 		'Admin User': 'my-cookie=1;Path=/;SameSite=strict',
 		'Normal User': 'my-cookie=0;Path=/;SameSite=strict',
-		'My JWT': jwtCookie('my-cookie', { foo: 'bar' })
+		'My JWT': jwtCookie('my-cookie', {
+			email: 'john.doe@example.com',
+			picture: 'https://cdn.auth0.com/avatars/jd.png'
+		})
 	}
 })

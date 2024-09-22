c57c629518fb3f2533e44b4352442bb1509e7bd5
Eric Fortis <efortis@gmail.com>
2024-09-22 11:29:58 -0400

    refactor

--- src/Mockaton.js
+++ src/Mockaton.js
@@ -11,31 +11,33 @@ import { apiPatchRequests, apiGetRequests } from './Api.js'
 export function Mockaton(options) {
 	setup(options)
 	mockBrokerCollection.init()
-
-	return createServer(async (req, response) => {
-		response.setHeader('Server', 'Mockaton')
-
-		const { url, method } = req
-		if (method === 'GET' && apiGetRequests.has(url))
-			apiGetRequests.get(url)(req, response)
-
-		else if (method === 'PATCH' && apiPatchRequests.has(url))
-			await apiPatchRequests.get(url)(req, response)
-
-		else if (isStatic(req))
-			await dispatchStatic(req, response)
-
+	const server = createServer(onRequest)
+	server.listen(Config.port, Config.host, (error) => {
+		const { address, port } = server.address()
+		const url = `http://${address}:${port}`
+		console.log('Listening', url)
+		console.log('Dashboard', url + API.dashboard)
+		if (error)
+			console.error(error)
 		else
-			await dispatchMock(req, response)
+			Config.onReady(url + API.dashboard)
 	})
-		.listen(Config.port, Config.host, function (error) {
-			const { address, port } = this.address()
-			const url = `http://${address}:${port}`
-			console.log('Listening', url)
-			console.log('Dashboard', url + API.dashboard)
-			if (error)
-				console.error(error)
-			else
-				Config.onReady(url + API.dashboard)
-		})
+	return server
+}
+
+async function onRequest(req, response) {
+	response.setHeader('Server', 'Mockaton')
+
+	const { url, method } = req
+	if (method === 'GET' && apiGetRequests.has(url))
+		apiGetRequests.get(url)(req, response)
+
+	else if (method === 'PATCH' && apiPatchRequests.has(url))
+		await apiPatchRequests.get(url)(req, response)
+
+	else if (isStatic(req))
+		await dispatchStatic(req, response)
+
+	else
+		await dispatchMock(req, response)
 }

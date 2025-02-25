7d26eada546ed8df7fc89552a57775ba83dc6ad0
Eric Fortis <efortis@gmail.com>
2025-02-24 20:44:27 -0400

    refactor

--- src/MockBroker.js
+++ src/MockBroker.js
@@ -3,7 +3,7 @@ import { DEFAULT_500_COMMENT, DEFAULT_MOCK_COMMENT } from './ApiConstants.js'
 
 
 // MockBroker is a state for a particular route. It knows the available mock files
-// that can be served for the route, the currently selected file, and its delay.
+// that can be served for the route, the currently selected file, and if it’s delayed.
 export class MockBroker {
 	constructor(file) {
 		this.urlMaskMatches = new UrlMatcher(file).urlMaskMatches
@@ -15,20 +15,44 @@ export class MockBroker {
 		this.register(file)
 	}
 
+	get file() { return this.currentMock.file }
+	get status() { return parseFilename(this.file).status }
+	get delayed() { return this.currentMock.delayed }
+	get proxied() { return !this.currentMock.file }
+	get temp500IsSelected() { return this.#isTemp500(this.file) }
+
+	hasMock(file) { return this.mocks.includes(file) }
+
 	register(file) {
 		if (parseFilename(file).status === 500) {
-			this.#deleteTemp500()
 			if (this.temp500IsSelected)
 				this.updateFile(file)
+			this.#deleteTemp500()
 		}
 		this.mocks.push(file)
 		this.#sortMocks()
 	}
 
-	#deleteTemp500() {
-		this.mocks = this.mocks.filter(file => !this.#isTemp500(file))
+	#deleteTemp500() { this.mocks = this.mocks.filter(file => !this.#isTemp500(file)) }
+
+	#isTemp500(file) { return includesComment(file, DEFAULT_500_COMMENT) }
+
+	#sortMocks() {
+		this.mocks.sort()
+		const defaults = this.mocks.filter(file => includesComment(file, DEFAULT_MOCK_COMMENT))
+		const temp500 = this.mocks.filter(file => includesComment(file, DEFAULT_500_COMMENT))
+		this.mocks = [
+			...defaults,
+			...this.mocks.filter(file => !defaults.includes(file) && !temp500.includes(file)),
+			...temp500
+		]
 	}
 
+	ensureItHas500() {
+		if (!this.#has500())
+			this.#registerTemp500()
+	}
+	#has500() { return this.mocks.some(mock => parseFilename(mock).status === 500) }
 	#registerTemp500() {
 		const { urlMask, method } = parseFilename(this.mocks[0])
 		const file = urlMask.replace(/^\//, '') // Removes leading slash
@@ -44,32 +68,17 @@ export class MockBroker {
 		return isEmpty
 	}
 
-	get file() { return this.currentMock.file }
-	get delayed() { return this.currentMock.delayed }
-	get proxied() { return !this.currentMock.file }
-	get status() { return parseFilename(this.file).status }
-	get temp500IsSelected() { return this.#isTemp500(this.file) }
-
-	#isTemp500(file) { return includesComment(file, DEFAULT_500_COMMENT) }
-
-	#sortMocks() {
-		this.mocks.sort()
-		const defaults = this.mocks.filter(file => includesComment(file, DEFAULT_MOCK_COMMENT))
-		const temp500 = this.mocks.filter(file => includesComment(file, DEFAULT_500_COMMENT))
-		this.mocks = [
-			...defaults,
-			...this.mocks.filter(file => !defaults.includes(file) && !temp500.includes(file)),
-			...temp500
-		]
-	}
-
 	selectDefaultFile() {
 		this.updateFile(this.mocks[0])
 	}
 
-	hasMock(file) { return this.mocks.includes(file) }
-	updateFile(filename) { this.currentMock.file = filename }
-	updateDelayed(delayed) { this.currentMock.delayed = delayed }
+	updateFile(filename) {
+		this.currentMock.file = filename
+	}
+
+	updateDelayed(delayed) {
+		this.currentMock.delayed = delayed
+	}
 
 	updateProxied(proxied) {
 		if (proxied)
@@ -91,14 +100,6 @@ export class MockBroker {
 		for (const file of this.mocks)
 			comments.push(...extractComments(file))
 		return comments
-	}
-
-	ensureItHas500() {
-		if (!this.#has500())
-			this.#registerTemp500()
-	}
-	#has500() {
-		return this.mocks.some(mock => parseFilename(mock).status === 500)
 	}
 }
 

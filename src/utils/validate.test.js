02e1a42ebaef1d8000cbe0b7edf844dd1d73c403
Eric Fortis <efortis@gmail.com>
2025-04-26 01:21:54 -0400

    refactor

--- src/utils/validate.test.js
+++ src/utils/validate.test.js
@@ -7,54 +7,41 @@ describe('validate', () => {
 	describe('optional', () => {
 		it('accepts undefined', () =>
 			doesNotThrow(() =>
-				validate({}, {
-					myOpt: optional(Number.isInteger)
-				})))
+				validate({}, { field: optional(Number.isInteger) })))
 
 		it('accepts falsy value regardless of type', () =>
 			doesNotThrow(() =>
-				validate({ myOpt: 0 }, {
-					myOpt: optional(Array.isArray)
-				})))
+				validate({ field: 0 }, { field: optional(Array.isArray) })))
 
 		it('accepts when tester func returns truthy', () =>
 			doesNotThrow(() =>
-				validate({ myOpt: [] }, {
-					myOpt: optional(Array.isArray)
-				})))
+				validate({ field: [] }, { field: optional(Array.isArray) })))
 
 		it('rejects when tester func returns falsy', () =>
 			throws(() =>
-				validate({ myOpt: 1 }, {
-					myOpt: optional(Array.isArray)
-				}), /config.myOpt=1 is invalid/))
+					validate({ field: 1 }, { field: optional(Array.isArray) }),
+				/config.field=1 is invalid/))
 	})
 
 	describe('is', () => {
 		it('rejects mismatched type', () =>
 			throws(() =>
-				validate({ myStr: 1 }, {
-					myStr: is(String)
-				}), /config.myStr=1 is invalid/))
+					validate({ field: 1 }, { field: is(String) }),
+				/config.field=1 is invalid/))
 
 		it('accepts matched type', () =>
 			doesNotThrow(() =>
-				validate({ myStr: 1 }, {
-					myStr: is(Number)
-				})))
+				validate({ field: '' }, { field: is(String) })))
 	})
 
 	describe('custom tester func', () => {
 		it('rejects mismatched type', () =>
 			throws(() =>
-				validate({ myInt: 0.1 }, {
-					myInt: Number.isInteger
-				}), /config.myInt=0.1 is invalid/))
+					validate({ field: 0.1 }, { field: Number.isInteger }),
+				/config.field=0.1 is invalid/))
 
 		it('accepts matched type', () =>
 			doesNotThrow(() =>
-				validate({ myInt: 1 }, {
-					myInt: Number.isInteger
-				})))
+				validate({ field: 1 }, { field: Number.isInteger })))
 	})
 })

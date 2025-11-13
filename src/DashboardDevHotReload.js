b9f960e0ed04ad2659b26c0ddd12c41f3b8cf751
Eric Fortis <efortis@gmail.com>
2025-11-13 00:59:35 -0400

    hot reload css

--- src/DashboardDevHotReload.js
+++ src/DashboardDevHotReload.js
@@ -6,17 +6,29 @@ async function longPoll() {
 	try {
 		const response = await fetch(API.watchHotReload)
 		if (response.ok) {
-			const editedFile = await response.json()
-			if (editedFile) 
+			const editedFile = await response.json() || ''
+			if (editedFile.endsWith('.css')) {
+				hotReloadCSS(editedFile)
+				longPoll()
+			}
+			else if (editedFile)
 				location.reload()
 			else
 				longPoll()
 		}
-		else 
+		else
 			throw response.statusText
 	}
 	catch (error) {
 		console.error('hot reload', error?.message || error)
 		setTimeout(longPoll, 3000)
+	}
+}
+
+function hotReloadCSS(editedFile) {
+	const link = document.querySelector(`link[href*="${editedFile}"]`)
+	if (link) {
+		const href = link.href.split('?')[0]
+		link.href = href + '?t=' + Date.now()
 	}
 }

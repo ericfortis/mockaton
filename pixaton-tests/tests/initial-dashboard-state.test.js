ccb1e325c63812b239367fa88d662f0c7c3955bc
Eric Fortis <efortis@gmail.com>
2025-11-13 23:55:26 -0400

    screenshot test responsive dashboard

--- pixaton-tests/tests/initial-dashboard-state.test.js
+++ pixaton-tests/tests/initial-dashboard-state.test.js
@@ -1,4 +1,10 @@
 import { testPixels } from '../utils.js'
 
 
-testPixels(import.meta.filename)
+testPixels(import.meta.filename, {
+	viewports: [
+		{ width: 1024, height: 800 },
+		{ width: 500, height: 720 },
+	]
+})
+	

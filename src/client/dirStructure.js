5a3208205907168a59cb7f224e5d2cf2b6adda02
Eric Fortis <efortis@gmail.com>
2026-04-13 10:49:11 -0400

    refactor

--- src/client/dirStructure.js
+++ src/client/dirStructure.js
@@ -1,83 +1,59 @@
-class TrieNode {
-	constructor() {
-		this.items = []
-		this.kids = new Map()
-	}
+function TrieNode() {
+	this.items = []
+	this.kids = new Map()
 }
 
 // TODO it should ignore query string
 
 /**
- * @typedef {{method: string, urlMask:string, children: BrokerLite[]}} BrokerLite
- * @param {BrokerLite[]} brokers
- * @returns {BrokerLite[]}
+ * @param {Partial<BrokerRowModel>[]} brokers
+ * @returns {Partial<BrokerRowModel>[]}
  */
 export function dirStructure(brokers) {
 	const root = new TrieNode()
 
-	for (let b of brokers) {
-		let curr = root
-		for (const seg of b.urlMask.split('/').filter(Boolean)) {
-			if (!curr.kids.has(seg))
-				curr.kids.set(seg, new TrieNode())
-			curr = curr.kids.get(seg)
+	for (const b of brokers) {
+		let node = root
+		for (const seg of b.urlMask.split('/')) {
+			if (!node.kids.has(seg))
+				node.kids.set(seg, new TrieNode())
+			node = node.kids.get(seg)
 		}
-		curr.items.push(b)
+		node.items.push(b)
 	}
 
 	const result = []
-	for (const child of root.kids.values())
-		result.push(...convertNode(child))
+	for (const c of root.kids.values())
+		result.push(...convert(c))
 
-	if (root.items.length) {
-		const elems = [root.items[0], ...result]
-		for (let i = 1; i < root.items.length; i++)
-			elems.push(root.items[i])
-
-		const parentNode = elems[0]
-		for (let i = 1; i < elems.length; i++)
-			parentNode.children.push(elems[i])
-
-		return [parentNode]
-	}
-
-	return result
+	return root.items.length
+		? [merge(root.items, result)]
+		: result
 }
 
-/**
- * Recursively converts a TrieNode into a flattened, nested array of objects.
- * Flattens the tree by having the first available route at a given directory level
- * act as the parent wrapper for the remaining items and subdirectories.
- *
- * @param {TrieNode} node
- * @returns {BrokerLite[]}
- */
-function convertNode(node) {
-	const childNodes = []
-	for (const child of node.kids.values())
-		childNodes.push(...convertNode(child))
+function convert(node) {
+	const children = []
+	for (const c of node.kids.values())
+		children.push(...convert(c))
 
-	const elems = []
-	if (node.items.length) {
-		elems.push(node.items[0])
-		elems.push(...childNodes)
-		for (let i = 1; i < node.items.length; i++)
-			elems.push(node.items[i])
-	}
-	else
-		elems.push(...childNodes)
+	const elems = node.items.length
+		? [node.items[0], ...children, ...node.items.slice(1)]
+		: children
 
 	if (!elems.length)
 		return []
 
-	const parentNode = elems[0]
-
-	if (node.items.length || !parentNode.children.length) {
-		for (let i = 1; i < elems.length; i++)
-			parentNode.children.push(elems[i])
-		return [parentNode]
+	const [head, ...rest] = elems
+	if (node.items.length || !head.children.length) {
+		head.children.push(...rest)
+		return [head]
 	}
-
 	return elems
 }
 
+function merge(items, children) {
+	const [head, ...rest] = items
+	if (children.length || rest.length)
+		head.children.push(...children, ...rest)
+	return head
+}

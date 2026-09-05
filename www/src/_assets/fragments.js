cb01fb5f266386ba0b3718b7f7ff6c6a137b9b62
Eric Fortis <efortis@gmail.com>
2026-09-04 21:14:48 -0400

    www fragment highlight - refactor

--- www/src/_assets/fragments.js
+++ www/src/_assets/fragments.js
@@ -6,7 +6,7 @@
 		const a = document.createElement('a')
 		a.id = '-' + buildId(h.innerText)
 		a.href = '#' + a.id
-		a.addEventListener('click', () => highlightSectionTitle(a.getAttribute('href')))
+		a.addEventListener('click', highlightSectionTitle)
 		h.prepend(a)
 	}
 
@@ -25,10 +25,10 @@
 	let highlightTimer = null
 
 	if (location.hash)
-		highlightSectionTitle(location.hash)
+		highlightSectionTitle.call(document.querySelector(location.hash))
 
-	function highlightSectionTitle(fragmentName) {
-		const el = document.querySelector(fragmentName)?.parentNode
+	function highlightSectionTitle(linkEl) {
+		const el = this?.parentNode
 		if (!el) return
 		if (elPendingHighlightOff) {
 			elPendingHighlightOff.classList.remove(cHighlight)
